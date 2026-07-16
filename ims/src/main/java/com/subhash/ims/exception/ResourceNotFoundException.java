c230aadadf793ae62030887e8fe758aeb303124a
SubhashRavindraSRIVATSA <srs.programming@gmail.com>
2026-07-16 23:49:02 +0200

    [ims-server] : added error response and exception hqndling

---
+++ ims/src/main/java/com/subhash/ims/exception/ResourceNotFoundException.java
@@ -0,0 +1,8 @@
+package com.subhash.ims.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
