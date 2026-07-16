0e3fc5e8f6d15e361d190ddc128223285d7c6d04
SubhashRavindraSRIVATSA <srs.programming@gmail.com>
2026-07-16 23:49:02 +0200

    [ims-server] : added error response and exception hqndling

---
+++ ims/src/main/java/com/subhash/ims/exception/UnauthorizedException.java
@@ -0,0 +1,8 @@
+package com.subhash.ims.exception;
+
+public class UnauthorizedException extends RuntimeException{
+
+    public UnauthorizedException(String message) {
+        super(message);
+    }
+}
